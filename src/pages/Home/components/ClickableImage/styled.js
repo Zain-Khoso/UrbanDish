70445eb993491297289970c5adc09dcdd0e8482c
Zain <zain.khoso.dev@gmail.com>
2023-12-30 21:17:53 +0500

    Testimonial Image Fix

--- src/pages/Home/components/ClickableImage/styled.js
+++ src/pages/Home/components/ClickableImage/styled.js
@@ -3,13 +3,12 @@ import styled from "styled-components";
 
 // Styles
 export const PersonImgWrapper = styled("div")`
-    position: ${({ $inlarge }) => ($inlarge ? "fixed" : "absolute")};
-    top: ${({ $inlarge }) => ($inlarge ? "50%" : "0")};
+    position: absolute;
+    top: 0;
     left: 50%;
 
-    width: ${({ $inlarge }) => ($inlarge ? "40%" : "4rem")};
+    width: 4rem;
     aspect-ratio: 1/1;
     border-radius: 50%;
     translate: -50% -50%;
-    z-index: ${({ $inlarge }) => $inlarge && 10};
 `;
